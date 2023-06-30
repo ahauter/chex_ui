2768cec0828d43505c0e2a849a3cb3e5f2f8fdc5
ahauter <hauterame.com>
2023-06-30 00:42:42 -0700

    fix repeat hexes

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -27,22 +27,32 @@ export default function Board({ width, height }) {
     const hexSize = 30
 
     const hexes = hexRange().map(
-        i => hexRange().map(
-            j => hexRange().map(
+        i => hexRange(i).map(
+            j => hexRange(i, j).map(
                 k => {
-                    const [x, y] = hexPosition(centerWidth, centerHeight, hexSize, i, j, k)
-                    return <Hex
-                        x={x} y={y}
-                        size={hexSize}
-                        fill={hexColor(i, j, k)}
-                        stroke='black'
-                        strokeWidth='1'
-                    />
+                    let minNum = Math.min(i, j, k);
+                    let co_i = i - minNum
+                    let co_j = j - minNum
+                    let co_k = k - minNum
+                    const [x, y] = hexPosition(centerWidth, centerHeight, hexSize, co_i, co_j, co_k)
+                    if (minNum < 1)
+                        return <Hex
+                            x={x} y={y}
+                            size={hexSize}
+                            fill={hexColor(i, j, k)}
+                            stroke='black'
+                            strokeWidth='1'
+                            onClick={() => console.log(co_i, co_j, co_k)}
+                            key={`${i}${j}${k}`}
+                        >
+                        </Hex>
+                    else return null;
                 }
             ).flat()
         ).flat()
-    ).flat();
-    console.log(hexes);
+    ).flat().filter(hex => hex);
+
+    console.log(hexes.length);
     return <div className={styles.board}>
         <svg
             ref={canvasRef}
